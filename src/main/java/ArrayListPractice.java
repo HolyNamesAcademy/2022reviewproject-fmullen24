64f3d6da5e02ce5fc2d0b4b7ba64848d6582f0af
fmullen24 <fmullen24@holynames-sea.org>
2022-09-20 11:41:35 -0700

    9/19/22

--- src/main/java/ArrayListPractice.java
+++ src/main/java/ArrayListPractice.java
@@ -3,20 +3,20 @@ import java.util.ArrayList;
 public class ArrayListPractice {
     /**
      * Get the first element in the ArrayList.
-     * 
+     *
      * @param numbers List of numbers to search over.
      * @return The first element in the list.
      */
     public static int GetFirst(ArrayList<Integer> numbers) {
         int j = 0;
-    for (int i = 0; i < 1; i++)
-        j += numbers.get(i);
-    return j;
+        for (int i = 0; i < 1; i++)
+            j += numbers.get(i);
+        return j;
     }
 
     /**
      * Get the third element in the ArrayList.
-     * 
+     *
      * @param numbers List of numbers to search over.
      * @return The third element in the list.
      */
@@ -30,20 +30,20 @@ public class ArrayListPractice {
 
     /**
      * Get the last element in the ArrayList.
-     * 
+     *
      * @param numbers List of numbers to search over.
      * @return The last element in the list.
      */
     public static int GetLast(ArrayList<Integer> numbers) {
         int j = 0;
-        for (int i = numbers.size()-1; i > numbers.size()-2; i--)
+        for (int i = numbers.size() - 1; i > numbers.size() - 2; i--)
             j += numbers.get(i);
         return j;
     }
 
     /**
      * Get the sum of all elements in the ArrayList.
-     * 
+     *
      * @param numbers List of numbers to search over.
      * @return The sum of the elements in the list.
      */
@@ -57,33 +57,33 @@ public class ArrayListPractice {
     /**
      * Returns a string that contains all of the numbers in the ArrayList
      * separated by one space.
-     * 
+     * <p>
      * For example, if numbers = {1, 4, 3, 2, 6}, your output should
      * be "1 4 3 2 6".
-     * 
+     *
      * @param numbers List of numbers to search over.
      * @return A string representing the list.
      */
     public static String ConvertToString(ArrayList<Integer> numbers) {
-String numberList = "";
-        for(int i = 0; i <numbers.size() - 1; i++)
-            numberList+=numbers.get(i) + " ";
-        numberList+=numbers.get(numbers.size()-1);
+        String numberList = "";
+        for (int i = 0; i < numbers.size() - 1; i++)
+            numberList += numbers.get(i) + " ";
+        numberList += numbers.get(numbers.size() - 1);
         return numberList;
     }
 
     /**
      * Returns the largest element in the array
-     * 
+     *
      * @param numbers List of numbers to search over.
      * @return The largest element in the list.
      */
     public static int GetMax(ArrayList<Integer> numbers) {
 
         int j = 0;
-        for (int i = 0; i < numbers.size()-1; i++)
-           if(numbers.get(i)>j)
-               j = numbers.get(i);
+        for (int i = 0; i < numbers.size() - 1; i++)
+            if (numbers.get(i) > j)
+                j = numbers.get(i);
         return j;
     }
 
@@ -92,19 +92,19 @@ String numberList = "";
      * Include the first element, but not the last one.
      * If the first and last elements are the same, then the list should be empty.
      * If the last number is less than the first, then the list should also be empty.
-     *
+     * <p>
      * For example:
-     *  CreateNumberArray(4, 8) returns {4, 5, 6, 7}
-     *  CreateNumberArray(2, 2) returns {} (an empty list)
-     * 
+     * CreateNumberArray(4, 8) returns {4, 5, 6, 7}
+     * CreateNumberArray(2, 2) returns {} (an empty list)
+     *
      * @param first The first number in the list (inclusive).
-     * @param last The last number in the list (exclusive).
+     * @param last  The last number in the list (exclusive).
      * @return A list of all numbers between first and last.
      */
     public static ArrayList<Integer> CreateNumberArray(int first, int last) {
 
         ArrayList<Integer> y = new ArrayList<Integer>();
-        for (int i = first; i<last; i++){
+        for (int i = first; i < last; i++) {
             y.add(i);
         }
         return y;
@@ -114,23 +114,23 @@ String numberList = "";
      * Finds the first student whose favorite color is the one specified.
      *
      * @param students The list of students to search over.
-     * @param color The favorite color the student must have.
+     * @param color    The favorite color the student must have.
      * @return The first student whose favorite color is the one specified.
      */
     public static Student GetFirstStudentWithFavoriteColor(ArrayList<Student> students, String color) {
-        for (int i = 0; i < students.size()-1; i++){
-            if(students.get(i).GetFavoriteColor().equals(color)){
+        for (int i = 0; i < students.size() - 1; i++) {
+            if (students.get(i).GetFavoriteColor().equals(color)) {
                 return students.get(i);
             }
         }
-           return students.get(0);
+        return students.get(0);
     }
 
     /**
      * Gets the favorite color for a specified student.
      *
      * @param students The list of students to search over.
-     * @param name The name of the student to look for.
+     * @param name     The name of the student to look for.
      * @return The favorite color of the specified student.
      */
     public static String GetFavoriteColorOfStudent(ArrayList<Student> students, String name) {
@@ -142,6 +142,7 @@ String numberList = "";
         }
         return students.get(0).GetFavoriteColor();
     }
+
     /**
      * Finds the tallest student.
      *
@@ -151,131 +152,158 @@ String numberList = "";
     public static Student GetTallestStudent(ArrayList<Student> students) {
         for (int i = 0; i < students.size() - 1; i++) {
             int height = 0;
-            if (students.get(i).GetHeight()>height) {
-               height = students.get(i).GetHeight();
-               return students.get(i);
+            if (students.get(i).GetHeight() > height) {
+                height = students.get(i).GetHeight();
+                return students.get(i);
             }
         }
         return students.get(0);
     }
 
     /**
-     *  Creates an array of Student objects using the information provided in via the parameters.
+     * Creates an array of Student objects using the information provided in via the parameters.
      * (names[0], heights[0] ...) are for the first student. (names[1], height[1]...)
      * are for the second student, etc.
-     * 
-     * @param names The list of names of the students.
-     * @param heights The list of heights of the students.
-     * @param gradeLevels The list of gradeLevels of the students.
+     *
+     * @param names          The list of names of the students.
+     * @param heights        The list of heights of the students.
+     * @param gradeLevels    The list of gradeLevels of the students.
      * @param favoriteColors The list of favoriteColors of the students.
-     * @param bankAccounts The list of bankAccounts of the students.
+     * @param bankAccounts   The list of bankAccounts of the students.
      * @return List of Student objects.
      */
     public static ArrayList<Student> CreateStudentArray(ArrayList<String> names, ArrayList<Integer> heights, ArrayList<Integer> gradeLevels, ArrayList<String> favoriteColors, ArrayList<BankAccount> bankAccounts) {
-       ArrayList<Student> students = new ArrayList<Student>();
-       for (int i =0;i<names.size();i++){
-           Student x = new Student (names.get(i),heights.get(i),gradeLevels.get(i),favoriteColors.get(i), bankAccounts.get(i));
-         students.add(x);}
-       return students;
+        ArrayList<Student> students = new ArrayList<Student>();
+        for (int i = 0; i < names.size(); i++) {
+            Student x = new Student(names.get(i), heights.get(i), gradeLevels.get(i), favoriteColors.get(i), bankAccounts.get(i));
+            students.add(x);
+        }
+        return students;
     }
 
     /**
      * Returns a string that lists teams from a 2-D ArrayList. Each ArrayList in
      * the bigger ArrayList contains members of the team.
-     * 
+     * <p>
      * The expected output looks like this:
      * Team 1: Amy, Abby, Arnold
      * Team 2: Bob, Beatrice, Becky, Ben
      * Team 3: Claire
-     * 
+     *
      * @param teams The list of teams of students.
      * @return A string that lists the team number followed by each team.
      */
     public static String GetTeamsString(ArrayList<ArrayList<Student>> teams) {
 
-       String str = "";
-       String str2= new String();
-       int i = 0;
-       for (ArrayList<Student> a: teams){
-           str+= "Team "+ (i+1)+ ": ";
-           i++;
-             for(Student b : a)  {
-                 str+= b.GetName()+ ", "+ "\n";
-                 if(str.indexOf(",", str.length()-1)!=-1){
-                     str2 = str.substring(0,str.length()-1);
-                 }
-             }
-           }
-       if(str2.length()>0)
-        return str2;
-       return str;
+        String str = "";
+        String str2 = new String();
+        int i = 0;
+        for (ArrayList<Student> a : teams) {
+            str += "Team " + (i + 1) + ": ";
+            i++;
+            for (Student b : a) {
+                str += b.GetName() + ", " + "\n";
+                if (str.indexOf(",", str.length() - 1) != -1) {
+                    str2 = str.substring(0, str.length() - 1);
+                }
+            }
+        }
+        if (str2.length() > 0)
+            return str2;
+        return str;
     }
 
     /**
      * Finds the student with the given name, and updates their favorite color to the given color.
      *
-     * @param students The list of students to search over.
-     * @param name The name of the student whose favorite color should be updated.
+     * @param students         The list of students to search over.
+     * @param name             The name of the student whose favorite color should be updated.
      * @param newFavoriteColor The new favorite color of the student.
      */
     public static void UpdateFavoriteColor(ArrayList<Student> students, String name, String newFavoriteColor) {
 
-        // write your code above and remove the line below
-        throw new UnsupportedOperationException();
+        for (int i = 0; i < students.size() - 1; i++) {
+            if (students.get(i).GetName().equals(name)) {
+                students.get(i).SetFavoriteColor(newFavoriteColor);
+            }
+        }
     }
 
     /**
      * Create an ArrayList consisting of all students in the given grade level.
      *
-     * @param students The list of students to search over.
+     * @param students   The list of students to search over.
      * @param gradeLevel The grade level the students should be in.
      * @return An ArrayList containing all the students in gradeLevel.
      */
     public static ArrayList<Student> GetStudentsInGradeLevel(ArrayList<Student> students, int gradeLevel) {
 
-        // write your code above and remove the line below
-        throw new UnsupportedOperationException();
+        ArrayList<Student> organizedStudents = new ArrayList<Student>();
+        for (int i = 0; i < students.size() - 1; i++) {
+            if (students.get(i).GetGradeLevel() == gradeLevel) {
+                organizedStudents.add(students.get(i));
+            }
+        }
+        return organizedStudents;
     }
 
     /**
      * Transfer money from one student to another.
      *
-     * @param students A list of students.
+     * @param students        A list of students.
      * @param fromStudentName The name of the student to transfer money from.
-     * @param toStudentName The name of the student to transfer money to.
-     * @param amount The amount of money to transfer.
+     * @param toStudentName   The name of the student to transfer money to.
+     * @param amount          The amount of money to transfer.
      * @return true if the transfer was successful, i.e. the student to transfer money from
-     *     had sufficient funds in their account. Otherwise, false.
+     * had sufficient funds in their account. Otherwise, false.
      */
     public static boolean TransferMoney(ArrayList<Student> students, String fromStudentName, String toStudentName, double amount) {
+        int n = 0;
+        for (int j = 0; j < students.size() - 1; j++) {
+            if (students.get(j).GetName().equals(toStudentName)) {
+                n = j;
+            }
+        }
+        for (int i = 0; i < students.size() - 1; i++) {
+            if (students.get(i).GetName().equals(fromStudentName)) {
+                if (students.get(i).GetBankAccount().GetBalance() > amount || students.get(i).GetBankAccount().GetBalance() == amount) {
+                    students.get(i).GetBankAccount().Withdraw(amount);
+                    students.get(n).GetBankAccount().Deposit(amount);
+                    return true;
+                }
+            }
+            return false;
+        }
 
-        // write your code above and remove the line below
-        throw new UnsupportedOperationException();
-    }
+        /**
+         * EXTRA CREDIT:
+         * Increments everyone's grade level by one. If a student is in 12th grade, remove them from the array.
+         *
+         * @param students The list of students to advance to the next grade.
+         */
 
-    /**
-     * EXTRA CREDIT:
-     * Increments everyone's grade level by one. If a student is in 12th grade, remove them from the array.
-     *
-     * @param students The list of students to advance to the next grade.
-     */
-    public static void UpdateGradeLevels(ArrayList<Student> students) {
+        public static void UpdateGradeLevels(ArrayList<Student> students) {
+            for (int i = students.size() -1; i >= 0; i--) {
+                if(students.get(i).GetGradeLevel()==12){
+                    students.remove(i);
+                }
+                else{
+                    students.get(i).SetGradeLevel(students.get(i).GetGradeLevel() + 1);
+                }
+            }
+        }
+        /**
+         * EXTRA CREDIT:
+         * Sort the array list by increasing grade level (all the 9th graders, then 10th graders, etc)
+         * If there is a tie (aka 2 people in the same grade), the person whose name is first in the
+         * alphabet should appear first.
+         *
+         * @param students The list of students to sort.
+         */
+        public static void SortByGradeAndName (ArrayList<Student> students) {
 
-        // write your code above and remove the line below
-        throw new UnsupportedOperationException();
-    }
-
-    /**
-     * EXTRA CREDIT:
-     * Sort the array list by increasing grade level (all the 9th graders, then 10th graders, etc)
-     * If there is a tie (aka 2 people in the same grade), the person whose name is first in the
-     * alphabet should appear first.
-     *
-     * @param students The list of students to sort.
-     */
-    public static void SortByGradeAndName(ArrayList<Student> students) {
-
-        // write your code above and remove the line below
-        throw new UnsupportedOperationException();
+            // write your code above and remove the line below
+            throw new UnsupportedOperationException();
+        }
     }
 }
